2a9a438587be77bb93d4912ef0c66611b949ad40
高村美紀 <takamuraminori@koumuramikinoMacBook-Pro.local>
2018-08-29 00:18:12 +0900

    jqueryまだ

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,11 +1,24 @@
 class MembersController < ApplicationController
   before_action :authenticate_user!
+
   def index
-  	@members = current_user.members
+  	@days = []
+  	@t = Time.now
   end
 
   def show
-  	@member = target_member params[:id]
+  	@memeber = target_member params[:id]
+  end
+
+  def list
+  	@members = target_date params[:date]
+  	@date = params[:date]
+  	@member = Member.new
+  end
+
+  def month
+  	@days = []
+  	@t = DateTime.parse(params[:date])
   end
 
   def new
@@ -15,7 +28,7 @@ class MembersController < ApplicationController
   def create
     @member = current_user.members.new member_params
     @member.save!
-    redirect_to members_url
+    redirect_to date_list_path(@member.date)
   end
 
   def edit
@@ -31,7 +44,7 @@ class MembersController < ApplicationController
   def destroy
     @member = target_member params[:id]
     @member.destroy
-    redirect_to members_url
+    redirect_to date_list_path(@member.date)
   end
 
   def cfg
@@ -40,6 +53,10 @@ class MembersController < ApplicationController
   private
   def target_member member_id
     current_user.members.where(id: member_id).take
+  end
+
+  def target_date member_date
+    current_user.members.where(date: member_date)
   end
 
   def member_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 	get 'members/cfg'
+	get 'members/month'
+	get 'members/list/:date', to: 'members#list', as: 'date_list'
     devise_for :users
     resources :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

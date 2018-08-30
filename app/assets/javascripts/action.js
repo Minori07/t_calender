a1fc461f964a927ffbb20ac88f418f3c7036a15f
高村美紀 <takamuraminori@koumuramikinoMacBook-Pro.local>
2018-08-31 02:43:51 +0900

    cate change

--- app/assets/javascripts/action.js
+++ app/assets/javascripts/action.js
@@ -8,11 +8,11 @@ $(function(){
 		$(".new_box").slideUp(300);
 	})
 
-	$('.n_memo').on('click',function(){
-		$(".m_new_box").slideDown(300);
-	})
+	// $('.n_memo').on('click',function(){
+	// 	$(".m_new_box").slideDown(300);
+	// })
 
-	$('.sbm').on('click',function(){
-		$(".m_new_box").slideUp(300);
-	})
+	// $('.sbm').on('click',function(){
+	// 	$(".m_new_box").slideUp(300);
+	// })
 })
